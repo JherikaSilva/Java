0902e33b2d0070266d67dff6379f2b1a993867fb
Jherika Silva <pereirajherika@gmail.com>
2025-10-27 18:58:16 -0300

    Exemplo do uso de Consumer

---
+++ src/ExemploConsumer.java
@@ -0,0 +1,10 @@
+import java.util.function.Consumer;
+
+public class ExemploConsumer {
+    public static void main(String[] args){
+        Consumer<String> imprimirNome=
+                nome -> System.out.println(nome);
+
+        imprimirNome.accept("Jherika");
+    }
+}
